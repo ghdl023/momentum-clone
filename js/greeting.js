a6b747d0f2a802cbe20be52de6079b95de0b10e3
22wg <gwee1216@naver.com>
2018-11-18 16:05:56 +0900

    Update greeting.js
    LocalStorage에 user가 있을때없을때 초기화면과 로컬저장소에 저장했을때 repaint하도록함

--- js/greeting.js
+++ js/greeting.js
@@ -1,22 +1,31 @@
 const form = document.querySelector(".greeting_form");
 const input = form.querySelector(".greeting_from-input");
-const user = document.querySelector(".greeting_user");
-const name = document.querySelector(".greeting_user-name");
+const text = document.querySelector(".greeting_text");
+const title = document.querySelector(".greeting_text-title");
 
 const HIDDEN_CN = "hidden";
+
+function paintGreeting(name){
+    form.classList.add(HIDDEN_CN);
+    text.classList.remove(HIDDEN_CN);
+    title.innerHTML = `Good afternoon, ${name}.`;
+}
 
 function saveUserName(event){
     event.preventDefault();
     // alert(input.value);
     localStorage.setItem('user',input.value);
+    paintGreeting(input.value);
 }
 
+
 function init(){
-    if(localStorage.getItem('user')){
+    let user = localStorage.getItem('user');
+    if(user){
         form.classList.add(HIDDEN_CN);  
-        name.innerHTML = `Good afternoon, ${localStorage.getItem('user')}.`;
+        paintGreeting(user);
     }else{
-        user.classList.add(HIDDEN_CN);
+        text.classList.add(HIDDEN_CN);
         form.addEventListener("submit",saveUserName);
     }
 }
